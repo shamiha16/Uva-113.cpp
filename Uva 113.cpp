bcd7f0073afa58eae1feec2dbc5b7a3acf4b9726
shamiha16 <155057582+shamiha16@users.noreply.github.com>
2024-01-18 20:21:57 +0600

    Add files via upload

---
+++ Uva 113.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    double n,p;
+    while(scanf("%lf\n%lf",&n,&p)!=EOF)
+    {
+         printf("%.0lf\n",pow(p,1.0/n));}
+         return 0;
+    }
+
